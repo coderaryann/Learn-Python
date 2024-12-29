b82f8b3e86a152fd1e983609d04a96468ba4719a
Aryan Jaiswal <kumararayan67@gmail.com>
2024-12-29 16:11:19 +0530

    Ch-02 Practice Set

---
+++ Chapter-02/practice-set.py
@@ -0,0 +1,40 @@
+# 01. Write a python program to add two numbers.
+print("Q. no. 1 solution")
+
+a = 34
+b = 80
+print(a + b)
+
+# 02. Write a python program to find remainder when a number is divided by z.
+print("Q. no. 2 solution")
+
+c = 45
+d = 4
+print("Remainder when a is divided by b is", c % d)
+
+# 03. Check the type of vaiable assigned using input() fuction.
+print("Q. no. 3 solution")
+
+e = input("Enter a number or word: ")
+print(type(e))
+
+# 04. Use comparison operator to find out whether a given variable is a greater than 'b' or not. Take a = 34 and b = 80
+print("Q. no. 4 solution")
+
+f = int(input("Enter a number: "))
+g = int(input("Enter a number: "))
+print("first no. is greater than second no. is", f > g)
+
+# 05. Write a python program to find an average of two numbers entered by the user.
+print("Q. no. 5 solution")
+
+h = int(input("Enter a number: "))
+i = int(input("Enter a number: "))
+print("The average of these two number is:", (h+i)/2)
+
+# 06. Write a python program to calculate the square of a number entered by the user.
+print("Q. no. 6 solution")
+
+j = int(input("Enter a number: "))
+# print(j * j)
+print(j**2)
