0de438e1e472feb39f1529ceb1d520864317429d
Aryan Jaiswal <kumararayan67@gmail.com>
2025-01-01 10:37:32 +0530

    Chapter - 07  ( loops )

---
+++ Chapter-07/04-for_loop_iterate.py
@@ -0,0 +1,14 @@
+# For Loop with Lists
+l = [1, 5, 3, 56, 76, 454]
+for i in l:
+    print(i)
+
+# For Loop with Tuples
+t = {3, 5, 7, 34, 334}
+for i in t:
+    print(i)
+
+# For Loop with Strings
+s = "String"
+for i in s:
+    print(i)
---
+++ Chapter-07/06-break_and_continue.py
@@ -0,0 +1,10 @@
+for i in range(20):
+    if i == 11:
+        break  # Exit the loop
+    print(i)
+
+
+for i in range(20):
+    if i == 11:
+        continue  # skip this iteration
+    print(i)
